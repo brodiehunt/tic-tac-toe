a485620781102fa110922463b6a280c95ed887f1
Brodie Hunt <62501585+brodiehunt@users.noreply.github.com>
2023-11-23 08:31:00 +1100

    First commit for tic-tac-toe project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <button className="square">X</button>
+  )
+}
+
+export default App
